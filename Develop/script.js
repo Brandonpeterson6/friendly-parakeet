3ea6abbd01c6a524e53b526e11f230b4f07cbb91
Brandonpeterson6 <brandonpeterson.006@gmail.com>
2022-01-08 12:57:48 -0700

    generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,43 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  
+  do{
+    var prompt = window.prompt('Enter the length of password (8-128)');
+    range = Number.parseInt(prompt);
+    if(isNaN(range)){
+      window.alert("please enter a number")
+    }
+    else{
+      console.log(range);
+      if(range >  7 && range < 129){
+        console.log(range);
+        validationFailed = false;
+      }
+      else{
+        window.alert("please enter within the numbers specified")
+      }
+    }
+    
+  }while(validationFailed)
+  validationFailed = true;
+
+  do{
+    numbers = window.confirm('Do you want numbers?')
+    lowercase = window.confirm('Do you want lowercase letters?')
+    uppercase = window.confirm('Do you want upppercase letters?')
+    characters = window.confirm('Do you want special characters?')
+    if(numbers || lowercase || uppercase || characters){
+      validationFailed = false;
+    }
+    else{
+      validationFailed = true;
+      window.alert("please select at least 1 character set")
+    }
+  } while(validationFailed);
+  return makePassword(range, characters, numbers, lowercase, uppercase);
+}
 
 
 // Add event listener to generate button
