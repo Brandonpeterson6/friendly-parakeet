1eb21307e3cf04f5f921527fe942c5ffad790a5e
Brandonpeterson6 <brandonpeterson.006@gmail.com>
2022-01-07 20:41:35 -0700

    added vars

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,25 @@
 // Assignment code here
-
+var validationFailed = true;
+  var range;
+  var numbers;
+  var lowercase;
+  var uppercase;
+  var characters;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("write password")
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
